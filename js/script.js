5fe128cd517bd5bb12fd7fcdfd9e46269bbf0827
Antoine Bondet <antoine.bondet@gmail.com>
2025-05-04 21:16:54 +0200

    Ajout d'une pop-up Règle

--- js/script.js
+++ js/script.js
@@ -1,16 +1,32 @@
 import Game from "./Game.js";
 
 window.onload = async () => {
-    const canvas = document.getElementById("myCanvas");
-    const game = new Game(canvas);
-    await game.init();
-    game.start();
-    document.getElementById("mapSelect")
-        .addEventListener("change", e => game.generateMap(e.target.value));
-    document.getElementById("reshuffleBtn")
+  
+  const canvas = document.getElementById("myCanvas");
+  const game   = new Game(canvas);
+  await game.init();
+  game.start();
+
+  
+  document.getElementById("mapSelect")
+    .addEventListener("change", e => game.generateMap(e.target.value));
+
+ 
+  document.getElementById("reshuffleBtn")
     .addEventListener("click", () => {
-        game.shuffle();
-        game.score-= 200;
-        if (game.score < 0) game.score = 0;
-    })
+      game.shuffle();
+      game.score -= 200;
+      if (game.score < 0) game.score = 0;
+    });
+
+  
+  const btn    = document.getElementById("rulesBtn");
+  const modal  = document.getElementById("rulesModal");
+  const closeX = modal.querySelector(".close");
+
+  btn.addEventListener("click",  () => modal.style.display = "flex");
+  closeX.addEventListener("click", () => modal.style.display = "none");
+  window.addEventListener("click", e => {
+    if (e.target === modal) modal.style.display = "none";
+  });
 };
